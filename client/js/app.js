7f309fedc9b00be0c5265878993da106f5d6c6c7
rupert <rupert.qing@gmail.com>
2016-06-01 14:13:23 +0800

    add webpack

---
+++ client/js/app.js
@@ -0,0 +1,18 @@
+import 'lodash'
+import '../css/common.scss'
+
+$('.sidebar')
+    .sidebar({
+        transition       : 'overlay',
+        mobileTransition : 'uncover'
+    })
+    .sidebar('attach events', '.launch.button, .view-ui, .launch.item, p')
+    .sidebar('toggle')
+    
+
+class app {
+    asdfs() {
+        
+    }
+}
+
